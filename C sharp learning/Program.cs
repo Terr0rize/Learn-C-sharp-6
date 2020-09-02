400d1d3a54c37f7c9423102e8dbff43cd8858ff6
kitsune <57214464+Terr0rize@users.noreply.github.com>
2020-09-02 18:24:49 +0400

    Add files via upload

---
+++ C sharp learning/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic; //массив, списки.
+using System.Globalization;
+
+namespace C_sharp_learning
+{
+    class MainClass
+    {
+        static void Main(string[] args) //метод срабатывает при старте программы
+        {          
+
+            int owo = 10;
+            int[] num = new int[owo];
+            Random rand = new Random();
+            for(int i = 0; i < num.Length; i++)
+            {
+                num[i] = rand.Next(-100, 50);
+            }
+            foreach (int el in num)
+            {              
+                Console.WriteLine(el);
+            }
+           
+            Console.ReadKey();
+        }
+    }
+}
